a9852c189e736ac44fc89bb7f6fd7f63e1209ce6
Max Flores <max@maxflores.com>
2023-01-11 08:18:53 -0600

    Meta Description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,9 @@ export default function Document() {
     <Html lang="en">
       <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet" />
       <link rel="icon" href="/images/dbi-icon.png" />
+      <meta name="description" content="Deep Blue Images is an island photography company in Grand Cayman that specializes in wedding photography in Grand Cayman, ocean photography, and real estate photography." />
+      <meta name='keywords' content='grand cayman photography, wedding photography grand cayman, photoshoots in grand cayman, diving photography grand cayman' />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <Head />
       <body >
         <Main />
